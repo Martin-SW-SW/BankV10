8516188f9597cc773bc7f71c6e188a5bef1f3c1e
Martin-SW-SW <62882502+Martin-SW-SW@users.noreply.github.com>
2020-03-30 13:14:37 +0200

    Initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,46 @@
+﻿namespace BankV10
+{
+    public class BankAccount
+    {
+        #region Instance fields
+        private double _balance;
+        #endregion
+
+        #region Constructor
+        public BankAccount()
+        {
+            _balance = 0.0;
+        }
+        #endregion
+
+        #region Properties
+        public double Balance
+        {
+            get { return _balance; }
+        }
+        #endregion
+
+        #region Methods
+        public void Deposit(double amount)
+        {
+            if  (amount >= 0)
+            _balance = _balance + amount;
+            else
+            System.Console.WriteLine("Negative Number");
+        }
+
+        public void Withdraw(double amount)
+        {
+        if (amount >= 0)
+    {
+            if (amount < _balance)
+            _balance = _balance - amount;
+            else
+            System.Console.WriteLine("Missing Ammount");
+    }
+            else
+            System.Console.WriteLine("Wrong Input");
+    } 
+        #endregion
+    }
+}
